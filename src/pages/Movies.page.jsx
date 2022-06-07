52bf2223418b9799e1adcb52ee5d0aa2fbf73e3e
ProfL3502 <ProfL3502@lab.aca>
2022-06-07 13:04:52 -0500

    Implementando conexion remota para obtener peliculas

--- src/pages/Movies.page.jsx
+++ src/pages/Movies.page.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import ContenedorTabs from "../components/ContenedorTabs.component"
 import MenuLateral from "../components/MenuLateral.component"
@@ -6,12 +6,26 @@ import MenuLateral from "../components/MenuLateral.component"
 import data from "../resources/data"
 
 const MoviesPage = () => {
-
-    // temporal
-    const listaPeliculas = data.data
-
     // Variables de estado
     const [modoVisualizacion, setModoVisualizacion] = useState(1)
+    const [listaPeliculas, setListaPeliculas] = useState([])
+
+
+    useEffect( () => {
+        const dataFetch = async () => {
+            const resp = await fetch("https://mcuapi.herokuapp.com/api/v1/movies")
+            const data = await resp.json()
+            setListaPeliculas(data.data)
+            //console.log(data)
+            return data
+        }
+        
+        dataFetch()
+    }, [])
+
+    // temporal
+    //const listaPeliculas = data.data
+
 
     return <div className="container">
         <h1>Marvel Cinematic Universe (MCU)</h1>
